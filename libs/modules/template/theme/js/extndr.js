d0d4f7bec7a8db60c0c11f89cb5160ac0d8c1408
Rick Burgess <github@rickburgess.me>
2012-10-30 16:56:59 -0400

    adding a get_url js equiv

--- libs/modules/template/theme/js/extndr.js
+++ libs/modules/template/theme/js/extndr.js
@@ -3,3 +3,14 @@ $(document).ready(function() {
 	$('a.colorbox').colorbox({'opacity':1});
 	
 });
+
+function get_url(path){
+
+    if (SYSTEM.SITE_ROOT != '') {
+        path = '/' + SYSTEM.SITE_ROOT + '/' + path;
+    } else {
+        path = '/' + path;
+    }
+
+    return '' . path.replace('//','/');
+}
